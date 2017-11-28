004df0d15f5a227d5ac1cb4c65dcc6a62a94be88
Enner Pérez <ennerperez@gmail.com>
2017-11-28 03:36:58 -0400

    Initial project

---
+++ MarkdownToolkit/Foms/FormAbout.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Toolkit.Forms
+{
+    public partial class FormAbout : Form
+    {
+        public FormAbout()
+        {
+            InitializeComponent();
+
+            Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+            labelAbout.Text = string.Format(labelAbout.Text,
+                ApplicationInfo.Description,
+                ApplicationInfo.Version.ToString(),
+                ApplicationInfo.CopyrightHolder);
+
+            richTextBoxChangeLog.Text = System.Text.Encoding.UTF8.GetString(Properties.Resources.CHANGELOG);
+            richTextBoxLicense.Text = System.Text.Encoding.UTF8.GetString(Properties.Resources.LICENSE);
+
+            pictureBoxIcon.Image = Icon.ToBitmap();
+
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void linkLabelWeb_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start((sender as LinkLabel).Text);
+        }
+
+        
+    }
+}
---
+++ MarkdownToolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MarkdownToolkit")]
+[assembly: AssemblyDescription("Markdown Toolkit for Devigners")]
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+[assembly: AssemblyCompany("Enner Pérez")]
+[assembly: AssemblyProduct("MarkdownToolkit")]
+[assembly: AssemblyCopyright("Copyright © Enner Pérez")]
+[assembly: AssemblyTrademark("MarkdownToolkit")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(true)]
+
+[assembly: Guid("b2ae9627-2fb1-43da-b20a-28dddc8e3d73")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
